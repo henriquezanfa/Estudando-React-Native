2bee36e77a7f4eccec562199041339687ff6eda2
Henrique Hadermeck <henriquezanferrari@gmail.com>
2019-02-11 16:08:21 -0200

    Alterando conteúdo de <Text> com <TextInput> e <Button>

---
+++ App.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import { AppRegistry, StyleSheet, Text, View, Dimensions, TextInput, Button, Alert } from 'react-native'
+
+export default class Hello extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      newText:''
+    };
+  }
+
+  func() {
+    if(this.state.newText.trim() == '') {
+      this.setState({text: ''});
+      return;
+    }
+    this.setState({text: 'EAE ' + this.state.newText});
+  }
+
+  render() {
+    let sWidth = (Dimensions.get('window').width)/3;
+    return (
+      <View style={styles.view}>
+        <Text style={styles.text}>{this.state.text}</Text>
+        <View style={styles.vieww}>
+          <View style={{width:sWidth, height:50, backgroundColor: 'powderblue'}} />
+          <View style={{width:sWidth, height:50, backgroundColor: 'skyblue'}} />
+          <View style={{width:sWidth, height:50, backgroundColor: 'steelblue'}} />
+        </View>
+        <TextInput  
+          style={styles.input} 
+          placeholder="Digite seu nome"
+          onChangeText={(newText) => this.setState({newText})} 
+        />
+        <Button 
+          onPress={() => this.func()}
+          title="Me toque"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  text: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  vieww: {
+    flexDirection: 'row',
+  },
+  view: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  input: {
+    fontSize: 20 ,
+    height:40,
+    padding: 10
+  }
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => Hello);
